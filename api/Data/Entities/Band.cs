7ed3101de7de993197bf39584dca22a9dbfc06c0
Myx <info@azaaxin.com>
2024-06-21 00:26:48 +0200

    Add efcore with needed tables

---
+++ api/Data/Entities/Band.cs
@@ -0,0 +1,17 @@
+﻿namespace api.Data.Entities
+{
+    public partial class Band
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? Image { get; set; }
+
+        public string? Genre { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public virtual ICollection<Role> Roles { get; set; } = new List<Role>();
+    }
+}
---
+++ api/Data/Entities/Song.cs
@@ -0,0 +1,23 @@
+﻿namespace api.Data.Entities
+{
+    public partial class Song
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public int UploadedBy { get; set; }
+
+        public int? Version { get; set; }
+
+        public int PreviousVersion { get; set; }
+
+        public string Url { get; set; } = null!;
+
+        public virtual Member UploadedByNavigation { get; set; } = null!;
+
+        public virtual ICollection<Vote> Votes { get; set; } = new List<Vote>();
+    }
+}
